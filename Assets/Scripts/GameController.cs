acdfc93d6c69e759addd181e362d4fb9396048dd
Maciej Kawka <46383143+maciejkawka@users.noreply.github.com>
2021-02-17 12:09:36 +0000

    Text

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public float firstStageTime;
-    public float secondStageTime;
-    public float transitionTime;
-
     public GameObject player;
     public GameObject player1;
-    
+
+    public GameObject playerText;
+    public GameObject playerText1;
+
+    int playersCounter;
 
 
+    void Start()
+    {
+        playersCounter = 2;
+    }
+    void Update()
+    {
+        if(playersCounter==1)
+        {
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.gameObject.SetActive(false);
+            
+        }
+    }
 
 
 
